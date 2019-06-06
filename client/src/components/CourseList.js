25308f365de3f40059264e9363489d3775bcc50b
Peter Malecki <peter.malecki@hotmail.co.uk>
2019-06-06 14:06:29 +0100

    Added client files

---
+++ client/src/components/CourseList.js
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import Grid from '@material-ui/core/Grid';
+import Text from '@material-ui/core/TextField';
+import * as contentful from 'contentful';
+import Course from '../components/Course';
+import TextField from '@material-ui/core/TextField';
+
+const SPACE_ID = 'lnpvkmwqk4sf';
+const ACCESS_TOKEN = '3q04QlhB6v91H1Z17Hcz7q-V6mKoLR9pXNrC1ps6Pes';
+
+const client = contentful.createClient({space: SPACE_ID, accessToken: ACCESS_TOKEN});
+
+
+class CourseList extends Component{
+    state = {
+        courses: [],
+        searchString: ''
+    }
+
+    constructor(){
+        super();
+        this.getCourses();
+
+    }
+
+
+    getCourses(){
+        client.getEntries({
+            content_type: 'course',
+            query: this.state.searchString
+        })
+        .then((response) => {
+            this.setState({
+               // courses: response.items
+            })
+        })
+        .catch((error) => {
+            console.log(`Error occurred while fetching data {error}`);
+        })
+    }
+
+    onSearchInputChange = (event) => {
+        if(event.target.value){
+            this.setState({
+                searchString: event.target.value
+            })
+        }
+        else{
+            this.setState({
+                searchString: ''
+            })
+        }
+
+        this.getCourses();
+    }
+
+    render(){
+        return (
+            <div>
+                {this.state.courses ? (
+                    <div>
+                        <TextField style={{padding: 24}}
+                                    id="searchInput"
+                                    placeholder="Search for courses"
+                                    margin="normal"
+                                    onChange={this.onSearchInputChange}  
+                        />
+                        <Grid container spacing={24} style={{padding: 24}}>
+                          {  
+                              this.state.courses.map(currentCourse => (
+                                <Grid item xs={12} sm={6} lg={4} xl={3}>
+                                    <Course course={currentCourse}/>
+                                </Grid>
+                            ))
+                        }
+
+                        </Grid>
+
+
+                    </div>
+                ) :
+                "No courses found"
+                }
+            </div>
+                
+        )
+    }
+
+}
+
+export default CourseList;
