8943e2acf4c1bc40bd4e3e39b9d2ce8a3eb5a8ec
Peter Malecki <peter.malecki@hotmail.co.uk>
2019-06-11 13:30:56 +0100

    The query readWeather will fetch the data

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 var express = require('express')
 const fetch = require('node-fetch');
+const tableHelper = require('./src/tableInformationHelpers');
 var app = express()
 
 app.listen(8081)
 
 console.log('server started')
-
-
 
 
 app.get('/readWeather', function(req, res){
@@ -15,8 +14,10 @@ app.get('/readWeather', function(req, res){
     fetch(url)
     .then(res => res.text())
     .then((body) => {
+	    const informationFromTable = tableHelper.collectInformationFromTheHTML(body);
         
-
+        console.log(informationFromTable);
+        res.send(JSON.stringify(informationFromTable));
     });
 
  });
